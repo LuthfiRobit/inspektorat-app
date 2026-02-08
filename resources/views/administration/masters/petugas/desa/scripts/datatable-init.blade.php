396978098fbf0957b94c3c1fdb9e1551b3731605
LuthfiRobit <luthfirobit@gmail.com>
2026-02-08 21:44:40 +0700

    change views & scripts ptgs desa

--- resources/views/administration/masters/petugas/desa/scripts/datatable-init.blade.php
+++ resources/views/administration/masters/petugas/desa/scripts/datatable-init.blade.php
@@ -5,7 +5,7 @@
         ajax: {
             url: '{{ route('administrator.master.petugas.desa.list') }}',
             type: 'GET',
-            data: function(d) {
+            data: function (d) {
                 d.filter_status = $('#filter_status').val();
                 d.filter_kecamatan = $('#filter_kecamatan').val();
                 d.filter_desa = $('#filter_desa').val();
@@ -14,55 +14,62 @@
             }
         },
         columns: [{
-                data: 'checkbox',
-                orderable: false,
-                searchable: false,
-                className: 'text-center align-middle',
-            },
-            {
-                data: 'aksi',
-                orderable: false,
-                searchable: false,
-                className: 'text-center align-middle',
-            },
-            {
-                data: 'nip',
-                name: 'nip',
-                className: 'text-start align-middle',
-            },
-            {
-                data: 'nama_lengkap',
-                name: 'nama_lengkap',
-                className: 'text-start align-middle',
-            },
-            {
-                data: 'no_telp',
-                name: 'no_telp',
-                className: 'text-start align-middle',
-            },
-            {
-                data: 'jabatan',
-                name: 'jabatan',
-                className: 'text-start align-middle',
-            },
-            {
-                data: 'instansi',
-                orderable: false,
-                searchable: true,
-                className: 'text-start align-middle',
-            },
-            {
-                data: 'unit_kerja',
-                name: 'unit_kerja',
-                className: 'text-start align-middle',
-            },
-            {
-                data: 'status',
-                name: 'status',
-                orderable: false,
-                searchable: false,
-                className: 'text-center align-middle',
-            },
+            data: 'checkbox',
+            orderable: false,
+            searchable: false,
+            className: 'text-center align-middle',
+        },
+        {
+            data: 'aksi',
+            orderable: false,
+            searchable: false,
+            className: 'text-center align-middle',
+        },
+        {
+            data: 'nip',
+            name: 'nip',
+            className: 'text-start align-middle',
+        },
+        {
+            data: 'nama_lengkap',
+            name: 'nama_lengkap',
+            className: 'text-start align-middle',
+        },
+        {
+            data: 'no_telp',
+            name: 'no_telp',
+            className: 'text-start align-middle',
+        },
+        {
+            data: 'jabatan',
+            name: 'jabatan',
+            className: 'text-start align-middle',
+        },
+        {
+            data: 'instansi',
+            orderable: false,
+            searchable: true,
+            className: 'text-start align-middle',
+        },
+        {
+            data: 'unit_kerja',
+            name: 'unit_kerja',
+            className: 'text-start align-middle',
+        },
+        {
+            data: 'user_status',
+            name: 'user_status',
+            orderable: false,
+            searchable: false,
+            className: 'text-center align-middle',
+        },
+        {
+            data: 'status',
+            name: 'status',
+            orderable: false,
+            searchable: false,
+            className: 'text-center align-middle',
+        },
         ],
         language: {
             searchPlaceholder: "Cari (min 4 karakter)...",
@@ -74,7 +81,7 @@
             info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
             infoEmpty: "Tidak ada data tersedia",
         },
-        drawCallback: function(settings) {
+        drawCallback: function (settings) {
             // Re-enable tooltip setiap kali tabel di-render ulang
             $('[data-bs-toggle="tooltip"]').tooltip();
         }
@@ -84,13 +91,13 @@
     //     if (query.length >= 4 || query.length === 0) table.search(query).draw();
     // }, 1000);
 
-    $('#example_filter input').unbind().on('input', function() {
+    $('#example_filter input').unbind().on('input', function () {
         optimizedSearch($(this).val());
     });
 
     $('#filter_status, #filter_kecamatan, #filter_desa, #filter_jabatan').change(() => table.ajax.reload());
 
-    $('#selectAll').on('click', function() {
+    $('#selectAll').on('click', function () {
         $('.table-checkbox:not(:disabled)').prop('checked', this.checked);
     });
 </script>
--- resources/views/administration/masters/petugas/desa/scripts/edit-handler.blade.php
+++ resources/views/administration/masters/petugas/desa/scripts/edit-handler.blade.php
@@ -1,15 +1,34 @@
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('.selectpicker').selectpicker();
 
         let dataId = window.location.pathname.split('/').pop();
         let url = '{{ route('administrator.master.petugas.desa.show', ':id') }}'.replace(':id', dataId);
 
-        AjaxHandler.sendGetRequest(url, function(response) {
+        AjaxHandler.sendGetRequest(url, function (response) {
             if (response.status === 200 && response.data) {
                 let d = response.data;
 
                 $('#editForm').attr('data-id', d.id_petugas);
+
+                $('#edit_nip').val(d.nip);
+                $('#edit_nama_lengkap').val(d.nama_lengkap);
+                $('#edit_usia').val(d.usia);
+                $('#edit_jabatan').val(d.jabatan).selectpicker('refresh');
+                $('#edit_unit_kerja').val(d.unit_kerja);
+                $('#edit_alamat').val(d.alamat);
+                $('#edit_no_telp').val(d.no_telp);
+                $('#edit_email').val(d.email);
+                $('#edit_status').val(d.status).selectpicker('refresh');
+
+                // Parsing tanggal agar sesuai dengan format input date (YYYY-MM-DD)
+                const formatDate = (dateString) => {
+                    if (!dateString) return '';
+                    return dateString.split('T')[0]; // Ambil bagian tanggal saja
+                };
+
+                $('#edit_tanggal_awal').val(formatDate(d.tanggal_awal));
+                $('#edit_tanggal_akhir').val(formatDate(d.tanggal_akhir));
 
                 $('#edit_kecamatan_id').val(d.kecamatan_id).selectpicker('refresh');
 
@@ -18,7 +37,7 @@
                     '#edit_kecamatan_id',
                     '#edit_desa_id',
                     '{{ route('administrator.master.desa.list-by-kecamatan', ':id') }}',
-                    function(item, selectedId) {
+                    function (item, selectedId) {
                         const selected = selectedId == item.id_desa ? 'selected' : '';
                         return `<option value="${item.id_desa}" ${selected}>${item.nama_desa}</option>`;
                     },
@@ -38,6 +57,12 @@
                     $('#link-container').empty();
                 }
 
+                if (d.user_status === 'active') {
+                    $('#edit_akses_login_active').prop('checked', true);
+                } else {
+                    $('#edit_akses_login_inactive').prop('checked', true);
+                }
+
                 $('.selectpicker').selectpicker('refresh');
             } else {
                 ResponseHandler.handleError("Data tidak ditemukan.");
@@ -48,16 +73,16 @@
 
 
 <script>
-    $("#editForm").on("submit", function(e) {
+    $("#editForm").on("submit", function (e) {
         e.preventDefault();
         let form = $(this);
         let dataId = form.attr('data-id');
         let url = '{{ route('administrator.master.petugas.desa.update', ':id') }}'.replace(':id',
             dataId);
 
-        AjaxHandler.sendUpdateRequest(url, this, function() {
+        AjaxHandler.sendUpdateRequest(url, this, function () {
             window.location.href = "{{ route('administrator.master.petugas.desa.index') }}";
-        }, function(response) {
+        }, function (response) {
             let errors = response.responseJSON?.data || {};
             ResponseHandler.handleValidationErrors(errors, form);
         });
